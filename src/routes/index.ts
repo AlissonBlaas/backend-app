26f64a08e0a0b29b43b639bef3812d13eaef8a5b
alissonblaas <alissoblaas@gmail.com>
2020-12-15 18:14:36 -0300

    validate credentials for auth user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRoutes from './users.routes';
 import appointmentsRouter from './appointments.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRoutes);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
